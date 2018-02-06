2ea3a11db98f908770f39c5e980f61377c3f135b
Massimo Iacolare <info@massimoiacolare.it>
2018-02-06 09:50:52 +0100

    Added PHP working version

---
+++ PHP2/test/BaseTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alessio
+ * Date: 06/02/2018
+ * Time: 08:23
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase {
+
+    public function testItWorks(){
+
+        $this->assertTrue(true);
+    }
+}
